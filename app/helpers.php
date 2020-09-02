ab87a9fedd1ac45c1369f90fc3624674b1506ffb
3116373546@qq.com <WYJ0309@163>
2020-09-02 17:07:02 +0800

    初步确定方案

--- app/helpers.php
+++ app/helpers.php
@@ -7,3 +7,78 @@ function substr_format($text, $length, $replace='..', $encoding='UTF-8')
     }
     return $text;
 }
+
+/**
+ * 分组
+ * 例子：menuGroup([[],[],[]],0,['pid','id']);
+ * @param $menuList
+ * @param int $pid
+ * @param array $keyArr
+ * @return array
+ */
+function menuGroup($menuList,$pid = 0,$keyArr=[]){
+    if(empty($menuList)){
+        return [];
+    }
+    $menuArr = [];
+    foreach($menuList as $value){
+        if($value[$keyArr[0]] == $pid){
+            $value['sub_menu'] = menuGroup($menuList,$value[$keyArr[1]],$keyArr);
+            $menuArr[] = $value;
+        }
+    }
+    return $menuArr;
+}
+
+/**
+ * 重置key-value 以二维数组中某个某个key为索引
+ * keyValue([[],[],[]],['id']);
+ * @param $originArr
+ * @param array $keyArr
+ * @return array
+ */
+function keyValue($originArr,$keyArr=[]){
+    if(empty($originArr)){
+        return [];
+    }
+    $returnArr = [];
+    foreach($originArr as $value){
+        $returnArr[$value[$keyArr[0]]][] = $value;
+    }
+    return $returnArr;
+}
+
+/**
+ * 组合数据 以上面两个函数的结果作为第一第二个参数，用两个参数数组中关联的key作为第三个参数
+ * @param array $menuList
+ * @param array $subArr
+ * @param string $key_match
+ * @return array
+ */
+function combineMenu($menuList=[],$subArr=[],$key_match='id'){
+    if(empty($menuList)){
+        return [];
+    }
+    $resArr = [];
+    foreach ($menuList as $value){
+        if(empty($subArr[$value[$key_match]])){
+            $value['sub_menu'] = combineMenu($value['sub_menu'],$subArr,$key_match);
+        }else{
+            $value['sub_attr'] = $subArr[$value[$key_match]];
+        }
+        $resArr[] = $value;
+    }
+    return $resArr;
+}
+//无限级寻找子数据并更新
+function searchSon($pid_str,$new_prefix){
+    $sonObj = DB::table('admin_depart')->where('part_pid_str','=',$pid_str)->get();
+    if($sonObj->isNotEmpty()){
+        foreach ($sonObj as $obj){
+            DB::table('admin_depart')->where('part_id','=',$obj->part_id)->update(['part_pid_str'=>$new_prefix.'-'.$obj->part_pid]);
+            self::searchSon($obj->part_pid_str.'-'.$obj->part_id,$new_prefix.'-'.$obj->part_pid);
+        }
+    }else{
+        return '';
+    }
+}
--- resources/views/front/nav.blade.php
+++ resources/views/front/nav.blade.php
@@ -8,9 +8,12 @@
         </li>
         <li>
             <a class="nav_head" href="{{ url('/image') }}">图库展示</a>
+            <a class="nav_head" href="{{ url('/image') }}">生活随记</a>
+            <a class="nav_head" href="{{ url('/image') }}">旅行记忆</a>
+            <a class="nav_head" href="{{ url('/image') }}">书法诗词</a>
         </li>
         <li>
-            <a class="nav_head" href="{{ url('/news') }}">美文欣赏</a>
+            <a class="nav_head" href="{{ url('/news') }}">古文观止</a>
             <a class="nav_body" href="/">黄帝内经</a>
             <a class="nav_body" href="/">封神演义</a>
         </li>
